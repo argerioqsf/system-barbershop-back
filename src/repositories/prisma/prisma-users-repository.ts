93e099c18d18ac25cb57b0ac130e60e737d36500
Argerio Queiroz <argerioaf3@gmail.com>
2024-04-05 18:35:22 -0300

    ajuste de retorno

--- src/repositories/prisma/prisma-users-repository.ts
+++ src/repositories/prisma/prisma-users-repository.ts
@@ -1,5 +1,5 @@
 import { prisma } from '@/lib/prisma'
-import { Prisma, User } from '@prisma/client'
+import { Prisma } from '@prisma/client'
 import { UsersRepository } from '../users-repository'
 
 export class PrismaUsersRepository implements UsersRepository {
@@ -16,6 +16,9 @@ export class PrismaUsersRepository implements UsersRepository {
     const user = await prisma.user.findUnique({
       where: {
         email,
+      },
+      include: {
+        profile: true,
       },
     })
     return user
