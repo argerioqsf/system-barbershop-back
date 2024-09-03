6cd60cb6bae34ecbe9b02504de563287dfc9dc1d
Matheus Mendes <matheus_mendess@live.com>
2024-09-03 16:22:43 -0300

    upload de imagem

--- src/lib/multer.ts
+++ src/lib/multer.ts
@@ -1,18 +1,16 @@
-import fastify from 'fastify'
-import multer from 'fastify-multer'
-const upload = multer({ dest: 'uploads/' })
+// import fastify from 'fastify'
+// import multer from 'fastify-multer'
+// const upload = multer({ dest: 'uploads/' })
 
-const server = fastify()
+// const server = fastify()
 
-server.register(multer.contentParser)
+// server.register(multer.contentParser)
 
-server.route({
-  method: 'POST',
-  url: '/profile',
-  preHandler: upload.single('avatar'),
-  handler: function (request, reply) {
-    // request.file is the `avatar` file
-    // request.body will hold the text fields, if there were any
-    reply.code(200).send('SUCCESS')
-  },
-})
+// server.route({
+//   method: 'POST',
+//   url: '/profile',
+//   preHandler: upload.single('avatar'),
+//   handler: function (request, reply) {
+//     reply.code(200).send('SUCCESS')
+//   },
+// })
