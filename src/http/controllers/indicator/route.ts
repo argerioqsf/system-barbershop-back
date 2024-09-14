7153b2153bb43b034ba28bf170c228ddf5c5ac3b
Argerio Queiroz <argerioaf3@gmail.com>
2024-09-14 16:59:41 -0300

    fix: deixado a rota de detalhes de um indicador publica para poder ser acessada da rota de link unico do indicador

--- src/http/controllers/indicator/route.ts
+++ src/http/controllers/indicator/route.ts
@@ -1,7 +1,6 @@
 import { verifyJWT } from '@/http/middlewares/verify-jwt'
 import { FastifyInstance } from 'fastify'
 import { List } from './list'
-import { GetIndicatorProfile } from './get-indicator'
 import { MountSelect } from './mount-select'
 import { UpdateActive } from './update-active'
 
@@ -9,8 +8,6 @@ export async function indicatorRoute(app: FastifyInstance) {
   app.addHook('onRequest', verifyJWT)
 
   app.get('/indicators', List)
-
-  app.get('/indicator/:id', GetIndicatorProfile)
 
   app.get('/indicator/select', MountSelect)
 
--- src/http/routes/route.ts
+++ src/http/routes/route.ts
@@ -3,10 +3,12 @@ import { authenticate } from '../controllers/authenticate-controller'
 import { registerUser } from '../controllers/register-user-controller'
 import { RegisterIndicatorController } from '../controllers/register-indicator-controller'
 import { RegisterLeadPublicController } from '../controllers/register-lead-public-controller'
+import { GetIndicatorProfile } from '../controllers/indicator/get-indicator'
 
 export async function appRoute(app: FastifyInstance) {
   app.post('/users', registerUser)
   app.post('/sessions', authenticate)
+  app.get('/indicator/:id', GetIndicatorProfile)
   app.post('/create/indicator', RegisterIndicatorController)
   app.post('/create/lead', RegisterLeadPublicController)
 }
