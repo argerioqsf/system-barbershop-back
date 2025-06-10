994260477076743a9c5ac035ecc947305ae36579
argerioqsf <39819614+argerioqsf@users.noreply.github.com>
2025-06-10 15:33:13 -0300

    fix: remove duplicated auth endpoints

--- src/http/routes/route.ts
+++ src/http/routes/route.ts
@@ -1,6 +1,4 @@
 import { FastifyInstance } from 'fastify'
-import { authenticate } from '../controllers/authenticate-controller'
-import { registerUser } from '../controllers/register-user-controller'
 import { RegisterIndicatorController } from '../controllers/register-indicator-controller'
 import { RegisterLeadPublicController } from '../controllers/register-lead-public-controller'
 import { GetIndicatorProfile } from '../controllers/indicator/get-indicator'
@@ -9,8 +7,6 @@ import { MountSelectUnit } from '../controllers/units/mount-select'
 import { MountSelectCourse } from '../controllers/courses/mount-select'
 
 export async function appRoute(app: FastifyInstance) {
-  app.post('/users', registerUser)
-  app.post('/sessions', authenticate)
   app.get('/indicator/:id', GetIndicatorProfile)
   app.post('/create/indicator', RegisterIndicatorController)
   app.post('/create/lead', RegisterLeadPublicController)
