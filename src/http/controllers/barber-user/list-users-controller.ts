f7b2657565003e8320dc81af0d6f9a4d5c544b61
argerioqsf <39819614+argerioqsf@users.noreply.github.com>
2025-06-10 19:03:52 -0300

    Include balance on barber user responses

--- src/http/controllers/barber-user/list-users-controller.ts
+++ src/http/controllers/barber-user/list-users-controller.ts
@@ -1,8 +1,17 @@
 import { makeListUsersService } from '@/services/@factories/barber-user/make-list-users'
+import { makeBarberBalance } from '@/services/@factories/report/make-barber-balance'
 import { FastifyReply, FastifyRequest } from 'fastify'
 
 export async function ListBarberUsersController(_: FastifyRequest, reply: FastifyReply) {
   const service = makeListUsersService()
   const { users } = await service.execute()
-  return reply.status(200).send(users)
+  const balanceService = makeBarberBalance()
+  const usersWithBalance = await Promise.all(
+    users.map(async (user) => {
+      const { balance } = await balanceService.execute({ barberId: user.id })
+      return { ...user, balance }
+    }),
+  )
+
+  return reply.status(200).send(usersWithBalance)
 }
