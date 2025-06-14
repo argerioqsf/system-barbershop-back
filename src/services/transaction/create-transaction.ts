6af25ed63b1de5645cf64222edaf8c553e66da0a
argerioqsf <argerioaf3@gmail.com>
2025-06-14 20:32:58 -0300

    fix: ajustes

--- src/services/transaction/create-transaction.ts
+++ src/services/transaction/create-transaction.ts
@@ -2,7 +2,7 @@ import { BarberUsersRepository } from '@/repositories/barber-users-repository'
 import { CashRegisterRepository } from '@/repositories/cash-register-repository'
 import { TransactionRepository } from '@/repositories/transaction-repository'
 import { ProfilesRepository } from '@/repositories/profiles-repository'
-import { Transaction, TransactionType } from '@prisma/client'
+import { Transaction, TransactionType, User } from '@prisma/client'
 import { UnitRepository } from '@/repositories/unit-repository'
 import { OrganizationRepository } from '@/repositories/organization-repository'
 
@@ -59,7 +59,9 @@ export class CreateTransactionService {
       amount: data.amount,
       session: { connect: { id: session.id } },
     })
+
     let surplusValue: number | undefined
+
     const increment = data.type === 'ADDITION' ? data.amount : -data.amount
     if (data.type === 'WITHDRAWAL' && data.amount < 0) {
       throw new Error('Negative values ​​cannot be passed on withdrawals')
@@ -67,19 +69,23 @@ export class CreateTransactionService {
     if (data.type === 'ADDITION' && data.amount < 0) {
       throw new Error('Negative values ​​cannot be passed on additions')
     }
+
     const effectiveUser = affectedUser ?? user
+    const balanceUnit = effectiveUser.unit?.totalBalance ?? 0
+    const balanceUser = effectiveUser.profile?.totalBalance ?? 0
+
     if (increment < 0) {
       // if (affectedUser) {
-      const balanceUnit = effectiveUser.unit?.totalBalance ?? 0
-      const balanceUser = effectiveUser.profile?.totalBalance ?? 0
       surplusValue =
         -increment > balanceUser
           ? balanceUser < 0
             ? increment
             : balanceUser - -increment
           : undefined
+
       const remainingBalance =
         balanceUser > 0 ? balanceUser - -increment : increment
+
       if (remainingBalance < 0) {
         if (!effectiveUser.unit?.allowsLoan) {
           throw new Error('Insufficient balance for withdrawal')
@@ -106,30 +112,36 @@ export class CreateTransactionService {
           increment,
         )
       }
-      // }
-      // else {
-      //   const balanceUnit = user.unit?.totalBalance ?? 0
-      //   if (-increment > balanceUnit && !user.unit?.allowsLoan) {
-      //     throw new Error('Withdrawal amount greater than unit balance')
-      //   }
-      //   await this.unitRepository.incrementBalance(user.unitId, increment)
-      //   await this.organizationRepository.incrementBalance(
-      //     user.organizationId,
-      //     increment,
-      //   )
-      // }
     } else {
-      if (affectedUser) {
+      if (balanceUser < 0) {
+        const remainingBalance = balanceUser - increment
+        const valueForPay = remainingBalance < 0 ? increment : balanceUser
+        await this.unitRepository.incrementBalance(
+          effectiveUser.unitId,
+          valueForPay,
+        )
+        await this.organizationRepository.incrementBalance(
+          effectiveUser.organizationId,
+          valueForPay,
+        )
         await this.profileRepository.incrementBalance(
-          affectedUser.id,
+          effectiveUser.id,
+          valueForPay,
+        )
+      } else {
+        await this.unitRepository.incrementBalance(
+          effectiveUser.unitId,
+          increment,
+        )
+        await this.organizationRepository.incrementBalance(
+          effectiveUser.organizationId,
+          increment,
+        )
+        await this.profileRepository.incrementBalance(
+          effectiveUser.id,
           increment,
         )
       }
-      await this.unitRepository.incrementBalance(user.unitId, increment)
-      await this.organizationRepository.incrementBalance(
-        user.organizationId,
-        increment,
-      )
     }
     return { transaction, surplusValue }
   }
