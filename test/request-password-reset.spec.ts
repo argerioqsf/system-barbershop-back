03f6d535afb25fa1980ae770abbebfc23d722987
argerioqsf <argerioaf3@gmail.com>
2025-06-16 15:54:01 -0300

    fix: ajuste

--- test/request-password-reset.spec.ts
+++ test/request-password-reset.spec.ts
@@ -1,8 +1,3 @@
-/* eslint-disable import/first */
-process.env.JWT_SECRET = 'test'
-process.env.PASSWORD_SEED = 'test'
-process.env.TOKEN_EMAIL_TWILIO = 'test'
-process.env.APP_WEB_URL = 'http://localhost:3000'
 import { describe, it, expect, beforeEach, vi } from 'vitest'
 import { RequestPasswordResetService } from '../src/services/users/request-password-reset'
 import { InMemoryUserRepository } from '../src/repositories/in-memory/in-memory-users-repository'
