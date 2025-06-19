8545e9819bb4790550eb37927725b6fc6e89c159
argerioqsf <39819614+argerioqsf@users.noreply.github.com>
2025-06-19 00:28:15 -0300

    Rewrite register user spec

--- test/tests/barber-user/register-user.spec.ts
+++ test/tests/barber-user/register-user.spec.ts
@@ -1,15 +1,21 @@
 import { describe, it, expect, beforeEach } from 'vitest'
 import { RegisterUserService } from '../../../src/services/barber-user/register-user'
-import { UserAlreadyExistsError } from '../../../src/services/@errors/user/user-already-exists-error'
-import { UnitNotExistsError } from '../../../src/services/@errors/unit/unit-not-exists-error'
-    await expect(
-      service.execute({ ...baseRegisterUserData, unitId: defaultUnit.id }),
-    ).rejects.toBeInstanceOf(UserAlreadyExistsError)
-    await expect(
-      service.execute({ ...baseRegisterUserData, unitId: 'x' }),
-    ).rejects.toBeInstanceOf(UnitNotExistsError)
+import {
+  InMemoryBarberUsersRepository,
   FakeUnitRepository,
 } from '../../helpers/fake-repositories'
+import { UnitNotExistsError } from '../../../src/services/@errors/unit/unit-not-exists-error'
+  let usersRepo: InMemoryBarberUsersRepository
+  let unitsRepo: FakeUnitRepository
+    usersRepo = new InMemoryBarberUsersRepository()
+    unitsRepo = new FakeUnitRepository({ ...defaultUnit }, [{ ...defaultUnit }])
+    service = new RegisterUserService(usersRepo, unitsRepo)
+    const res = await service.execute({
+      ...baseRegisterUserData,
+      unitId: defaultUnit.id,
+    })
+    expect(usersRepo.users).toHaveLength(1)
+    service = new RegisterUserService(usersRepo, badUnit)
 import { defaultUnit, baseRegisterUserData } from '../../helpers/default-values'
 
 describe('Register user service', () => {
