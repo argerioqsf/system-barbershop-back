6a3d831163df8586f8afaaa9c22c8ef396ecbfe4
argerioqsf <39819614+argerioqsf@users.noreply.github.com>
2025-06-24 13:29:02 -0300

    fix migration by dropping FK before removing day hour

--- prisma/migrations/20250627140000_blocked_hour_dates/migration.sql
+++ prisma/migrations/20250627140000_blocked_hour_dates/migration.sql
@@ -1,4 +1,5 @@
 -- AlterTable
+ALTER TABLE `profile_blocked_hours` DROP FOREIGN KEY `profile_blocked_hours_dayHourId_fkey`;
 ALTER TABLE `profile_blocked_hours` DROP COLUMN `dayHourId`, ADD COLUMN `startHour` DATETIME NOT NULL, ADD COLUMN `endHour` DATETIME NOT NULL;
 
 -- AlterTable
